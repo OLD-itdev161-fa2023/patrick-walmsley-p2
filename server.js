146cf8b864e42c0ca28151868164b7c1cdc8c35d
walmslea <98186467+walmslea@users.noreply.github.com>
2022-09-18 22:12:28 -0500

    Completed Activity 3

--- server.js
+++ server.js
@@ -5,8 +5,24 @@ const app = express();
 
 connectDatabase();
 
+app.use(express.json({extended: false}));
+/*
+@route 
+@desc
+*/
+
 app.get('/', (req, res) =>
 res.send('http get request sent to root api endpoint')
 );
+
+/*
+@route 
+@desc
+*/
+
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
 
 app.listen(3000, () => console.log('Express server running on port 3000'));
